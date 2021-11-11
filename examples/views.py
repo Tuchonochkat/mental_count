db3ece9a5eab967bc877b3dd3da47b530c656af3
yuvoinkova <yuvoinkova@gmail.com>
2021-11-11 19:02:53 +0300

    now its workng

--- examples/views.py
+++ examples/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+
 def main(request):
     return render(request, 'examples/main.html', {})
-# Create your views here.
+
+
 def block(request):
     return render(request, 'examples/block.html', {})
+
+
 def snake(request):
     return render(request, 'examples/snake.html', {})
